e0b36acbb0d64cc0606b17cd32b49964ee1c319d
ADITYA <adityapuri85@gmail.com>
2022-11-01 14:47:10 +0530

    Footer-style-done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
 import Home from "./components/Home/Home";
 import NotFound404 from "./components/NotFound404/NotFound404";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:imdbId" element={<Home />} />
-          <Route path="*" element={<NotFound404 />} />
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:imdbId" element={<Home />} />
+            <Route path="*" element={<NotFound404 />} />
+          </Routes>
+        </div>
         <Footer />
       </Router>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import "./Footer.scss";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>@Copyright-4d1ty4--Movies, Inc.</div>
+    </div>
+  );
 };
 
 export default Footer;
